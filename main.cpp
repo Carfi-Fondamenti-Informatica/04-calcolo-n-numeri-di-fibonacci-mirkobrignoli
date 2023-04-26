903b9f8cd33a32fa63978f25a6fef27bd06cc642
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-04-26 22:18:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,7 @@ int main()
     int n = 0,primo = 1,secondo = 1,terzo = 0;
     cout << "scegli il valore" << endl;
     cin >> n;
-    if(n <= 2){
-        cout << "errore" << endl;
-    }
-    else if (n > 2){
+    if (n >= 2){
         cout << primo << endl;
         cout << secondo <<endl;
         while (n-2 > 0){
@@ -22,6 +19,9 @@ int main()
 
             n--;
         }
+        else {
+        cout << "errore" << endl;
+    }
     }
     
     return 0;
