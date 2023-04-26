73387811e515e80830969243d407a5fa3c011b22
mirkobrignoli <127690671+mirkobrignoli@users.noreply.github.com>
2023-04-26 21:56:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
+
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+    int a = 0,primo = 1,secondo = 1,terzo = 0;
+    cout << "scegli il valore" << endl;
+    cin >> a;
+    if(a <= 2){
+        cout << "errore" << endl;
+    }
+    else if (a > 2){
+        cout << primo << endl;
+        cout << secondo <<endl;
+        while (a-2 > 0){
+            terzo = primo + secondo;
+            primo = secondo;
+            secondo = terzo;
+            cout << terzo << endl;
+            
+            a--;
+        }
+    }
+    
+    return 0;
 }
